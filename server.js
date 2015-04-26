4b7d9f59abfd7e82253f2a1ea4b692a5807120ee
Eltrien <Kwakch@yandex.ru>
2015-04-27 01:37:57 +0300

    27.04.2015.1:36
    3 zadanije (poka prostoy variant)

--- server.js
+++ server.js
@@ -1,44 +1,54 @@
 /**
  * Created by User on 24.04.2015.
  */
-//var tag_css = document.createElement('link');
-//tag_css.rel = 'stylesheet';
-//tag_css.href = 'theme.css'; // здесь указывается URL стилевого файла
-//tag_css.type = 'text/css';
-//var tag_head = document.getElementsByTagName('head');
-//tag_head[0].appendChild(tag_css);
 var http=require('http');
 var fs = require('fs');
 var url = require('url');
 
+
 var server=http.createServer(function(request,response)
 {
-    var url_parts = url.parse(request.url, true);
-    var query = url_parts.query;
-    //response.write("<link rel='stylesheet' href='css/style_js.css' type='text/css'>");
-    response.writeHead(200,{'Content-Type':'text/html'});
-    //var qwerty = url.format(request.parse(href));
-/*#РаботаетНеОч*/
+/*#VoTakLuchsheNoPokaTupo*/
     var pathname = url.parse(request.url).pathname;
     console.log(pathname);
+    /*--------------------------ot peremeni mest if izmenaetsa pravilnost raboti-------------------------------------*/
+    if (pathname == "/favicon.ico") response.end();
     if (pathname == "/index.html")
     {
         fs.readFile('index.html', function(err,contest)
         {
-            response.end(contest)
+            response.writeHead(200,{'Content-Type':'text/html'});
+            response.end(contest);
         })
     }
     if (pathname == "/forecast.html")
     {
         fs.readFile('forecast.html', function(err,contest)
         {
-            response.end(contest)
+            response.end(contest);
         })
     }
-/*#End РаботаетНеОч*/
-    //console.log(query);
+/*#UHTIJO*/
+    if (pathname == "/theme.css")/*vrode bi rabotaet, nuzhno bolshe testov*/
+    {
+        response.writeHead(200,{'Content-Type':'text/css'});
+        fs.readFile('theme.css', function(err,contest)
+        {
+            response.end(contest);
+        })
 
-
+    }
+/*#End UHTIJO*/
+/*#RabotaetNoCherezRaz:(*/
+    else/*na nepravilniy url vsegda gruzit errorpage, no inohda i na normalnie gruzit errorgage.. rabotaem!*/
+    {
+        fs.readFile('errorpage.html', function(err,contest)
+        {
+            response.end(contest);
+        })
+    }
+/*#End RabotaetNoCherezRaz:(*/
+/*#End VoTakLuchsheNoPokaTupo*/
 })
 server.listen(8000);
 console.log('server started');
