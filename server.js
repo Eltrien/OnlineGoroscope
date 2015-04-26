df8d038f30fbd74adf1932f9401d665a48c78133
Eltrien <Kwakch@yandex.ru>
2015-04-27 02:44:15 +0300

    27.04.2015.2:44
    dodelat proverku na nalichie faila

--- server.js
+++ server.js
@@ -4,41 +4,46 @@
 var http=require('http');
 var fs = require('fs');
 var url = require('url');
-
+var fso = new ActiveXObject("Scripting.FileSystemObject");
 
 var server=http.createServer(function(request,response)
 {
-/*#VoTakLuchsheNoPokaTupo*/
+    /*----------------------------------dinamich viborka stranic----------------------------------------*/
     var pathname = url.parse(request.url).pathname;
-    console.log(pathname);
-    /*--------------------------ot peremeni mest if izmenaetsa pravilnost raboti-------------------------------------*/
-    if (pathname == "/favicon.ico") response.end();
-    if (pathname == "/index.html")
+    var newpathname = "";
+    for (iq = 0; iq < pathname.length -1; iq++)
     {
-        fs.readFile('index.html', function(err,contest)
+        newpathname = newpathname + pathname[iq+1];
+    }
+    console.log("newpathname = " + newpathname);
+    if (newpathname == "favicon.ico") response.end();
+    /*var homeDir = Titanium.Filesystem.getUserDirectory(); ne robit*/
+    /*var mySampleFile = getFile(newpathname); ne robit*/
+
+/*sdelat proverku na nalichie faila*/
+
+
+    var ending = newpathname[(newpathname.length - 4)] + newpathname[(newpathname.length - 3)] + newpathname[(newpathname.length - 2)] + newpathname[(newpathname.length - 1)];
+    console.log("ending = " + ending);
+    if (ending == "html" && fso.FileExists(newpathname))
+    {
+        fs.readFile(newpathname, function(err,contest)
         {
             response.writeHead(200,{'Content-Type':'text/html'});
             response.end(contest);
         })
     }
-    if (pathname == "/forecast.html")
-    {
-        fs.readFile('forecast.html', function(err,contest)
-        {
-            response.end(contest);
-        })
-    }
-/*#UHTIJO*/
-    if (pathname == "/theme.css")/*vrode bi rabotaet, nuzhno bolshe testov*/
+    var ending = newpathname[(newpathname.length - 3)] + newpathname[(newpathname.length - 2)] + newpathname[(newpathname.length - 1)];
+    console.log("ending = " + ending);
+    if (ending == "css")
     {
         response.writeHead(200,{'Content-Type':'text/css'});
         fs.readFile('theme.css', function(err,contest)
         {
             response.end(contest);
         })
-
     }
-/*#End UHTIJO*/
+    /*----------------------------------dinamich viborka stranic----------------------------------------*/
 /*#RabotaetNoCherezRaz:(*/
     else/*na nepravilniy url vsegda gruzit errorpage, no inohda i na normalnie gruzit errorgage.. rabotaem!*/
     {
@@ -48,7 +53,6 @@ var server=http.createServer(function(request,response)
         })
     }
 /*#End RabotaetNoCherezRaz:(*/
-/*#End VoTakLuchsheNoPokaTupo*/
 })
 server.listen(8000);
 console.log('server started');
