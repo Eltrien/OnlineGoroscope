2b0ea4fe9c830812803e3c1ead7a4b9e145cc518
Eltrien <Kwakch@yandex.ru>
2015-05-02 15:21:55 +0300

    02.05.2015.15:21

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 /**
  * Created by User on 24.04.2015.
  */
-src="time.js";
 var http=require('http');
 var fs = require('fs');
 var url = require('url');
-var mysql      = require('mysql');
-var client = mysql.createClient();
-client.host='127.0.0.1';
-client.port= '3306';
+var clock = require('./time');
+/*var mysql = require('mysql');
+var host='127.0.0.1';
+var port= '3306';
+var client = mysql.createClient(port,host);
 client.user='root';
 client.password='qwerty007';
 client.database='node';
+
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
@@ -25,7 +26,7 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
     console.log('The solution is: ', rows[0].solution);
 });
 connection.end();
-
+*/
 var server=http.createServer(function(request,response)
 {
     var pathname = url.parse(request.url).pathname;
@@ -36,8 +37,21 @@ var server=http.createServer(function(request,response)
     }
     fs.readFile(newpathname,function read(err1,data)
     {
-        //var file = new Parse.FILE("");
-
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        var string = '<div id=\"top\"><ul><li><a href=\"index.html\"><img src=\"pictures/mainicon.jpg\" class=\"top-icon\"></a></li><li><a href=\"forecast.html\" class=\"top-links\">forecasts</a></li></ul></div>'
+        response.write(string.toString());
+        var s = clock();
+        string = '<div id=\"time\"><table><tr><td><span>'+s+'</span></td></tr></table></div>';
+        response.write(string.toString());
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        if (newpathname=="")
+        {
+            fs.readFile('index.html', function(err,contest)
+            {
+                response.end(contest);
+            })
+        }
         if(err1)
         {
             fs.readFile('errorpage.html', function(err,contest)
@@ -47,39 +61,12 @@ var server=http.createServer(function(request,response)
         }
         else
         {
-            var file = fs.readFile(newpathname, function (err, contest) {
-                console.log(file);
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                //response.replace(, "qwerty")
-                //response.end(contest);
-                var now = new Date();
-                var d = now.getUTCDate(); var dd = d; if(d<10) dd = '0'+d;
-                var m = now.getUTCMonth() + 1;	var mm = m; if(m<10) mm = '0'+m;
-                var yyyy = now.getUTCFullYear(); var yy = yyyy - 2000; if(yy<10) yy = '0'+yy;
-                var h = now.getHours(); var hh = h; if(h<10) hh='0'+h;
-                var n = now.getUTCMinutes(); var nn = n; if(nn<10) nn='0'+n;
-                var s = now.getUTCSeconds(); var ss = s; if(ss<10) ss='0'+s;
-                var s = new String('%hh:%nn <br> %dd/%mm/%yyyy');
-                s = s.replace( new RegExp("%dd"), dd);
-                s = s.replace( new RegExp("%mm"), mm);
-                s = s.replace( new RegExp("%yyyy"), yyyy);
-                s = s.replace( new RegExp("%hh"), hh);
-                s = s.replace( new RegExp("%nn"), nn);
-                s = s.replace( new RegExp("%ss"), ss);
-                var string = '<div id=\"time\">'+
-                    '<table' + /*' border=\"0\" bgcolor=\"#cfba6b\" cellspacing=1 cellpadding=3 class=\"clock24st\" style=\"line-height:25px; padding:0; height: 100px;\"' + */'>'+
-                    '<tr>'+
-                    '<td' + /*' bgcolor=\"#050020\" class=\"clock24std\"' + */'>'+
-                    '<span ' + /*'class=\"clock24\" id=\"clock24\" style=\"color:#cfba6b;\"' + */'>'+s+'</span></td></tr>'+
-                    '</table>'+
-                    '</div>';
-                response.write(string.toString());
-                response.end(contest);
-            })
-
+            response.end(data);
         }
     })
-    /*----------------------------------dinamich viborka stranic----------------------------------------*/
 })
 server.listen(8000);
 console.log('server started');
+
+
+
--- time.js
+++ time.js
@@ -1,42 +1,19 @@
-/**
- * Created by User on 27.04.2015.
- */
-//Copyright 2008 WWW.24WEBCLOCK.COM
-// v1.2
-// Javascript 1.3
-
-function clock24(fmt)
-{
-    this.fmt = fmt;
-    this.refresh = clock24_refresh;
-    this.format = clock24_format;
-    window.setInterval("clock24.refresh()", 1000);
-}
-
-function clock24_refresh()
+var myClock = function()
 {
     var now = new Date();
-    now = new Date( now.getTime());
-    document.getElementById('clock24').innerHTML = this.format(now, this.fmt);
-}
-
-function clock24_format(now, clock24_f)
-{
     var d = now.getUTCDate(); var dd = d; if(d<10) dd = '0'+d;
     var m = now.getUTCMonth() + 1;	var mm = m; if(m<10) mm = '0'+m;
     var yyyy = now.getUTCFullYear(); var yy = yyyy - 2000; if(yy<10) yy = '0'+yy;
     var h = now.getHours(); var hh = h; if(h<10) hh='0'+h;
     var n = now.getUTCMinutes(); var nn = n; if(nn<10) nn='0'+n;
     var s = now.getUTCSeconds(); var ss = s; if(ss<10) ss='0'+s;
-    var s = new String(clock24_f);
+    var s = new String('%hh:%nn <br> %dd/%mm/%yyyy');
     s = s.replace( new RegExp("%dd"), dd);
     s = s.replace( new RegExp("%mm"), mm);
     s = s.replace( new RegExp("%yyyy"), yyyy);
     s = s.replace( new RegExp("%hh"), hh);
     s = s.replace( new RegExp("%nn"), nn);
     s = s.replace( new RegExp("%ss"), ss);
-    return s.toString();
-}
-
-var clock24 = new clock24('%hh:%nn:%ss %dd/%mm/%yyyy');
-clock24.refresh();
+    return s;
+};
+module.exports = myClock;
